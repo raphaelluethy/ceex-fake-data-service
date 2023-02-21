6d75990381c403fa56a629179153c6fb9e50c602
Raphael Lüthy <raphael@famluethy.ch>
2023-02-21 15:37:53 +0100

    Added Jetbrains config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<h1>CEEX Fake Data Service</h1>
-		</>
-	);
+    return (
+        <>
+            <h1>CEEX Fake Data Service</h1>
+            <h2>Developed by @raphaelluethy</h2>
+        </>
+    );
 };
 
 export default Home;
