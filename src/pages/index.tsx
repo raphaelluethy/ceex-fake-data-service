c15de2210c87b17dd7574eacd74c7646a6b627cf
Raphael Lüthy <raphael@luethy.io>
2023-08-25 13:47:51 +0200

    Update homepage title for clarity
    The title on the homepage was modified to 'CEEX Fake Data Service for Testing' for better understanding of its purpose. This change provides clearer context to visitors about the function of the page/service.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
     return (
         <>
-            <h1>CEEX Fake Data Service</h1>
+            <h1>CEEX Fake Data Service for Testing</h1>
             <h2>Developed by @raphaelluethy</h2>
             ©raphaelluethy 2022-2023 - All rights reserved
         </>
